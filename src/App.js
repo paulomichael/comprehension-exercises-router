e5cbf6f3f2dcb2ec8560efa1e56f80c3040a5ade
paulomichael <paulo.michael@gmail.com>
2021-07-30 18:25:41 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 
+/* first change for the initial commit */
 class App extends Component {
   render() {
     return (
