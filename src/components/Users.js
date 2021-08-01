89f0de913af56a33fab0dc554e79f97343f11c32
paulomichael <paulo.michael@gmail.com>
2021-07-31 21:43:35 -0300

    exercise 10

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,14 +4,13 @@ class Users extends Component {
   render() {
     const {greetingMessage,
       match: {params: {id}}} = this.props
-    console.log(this.props.match.params);
-    console.log('console.log');
     if(id) {
       return (
         <div>
           <h2>Users</h2>
           <p>
-             {`${greetingMessage} Id ${id}`}, My awesome Users component </p>
+             {`${greetingMessage} Id ${id}`}, My awesome Users component
+          </p>
         </div>
       );
   
@@ -20,7 +19,8 @@ class Users extends Component {
       <div>
         <h2>Users</h2>
         <p>
-           All Users Component. </p>
+           All Users Component.
+        </p>
       </div>
     );
   }
